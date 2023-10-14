4a9b24df22554e492fe32ef51537ee794a9a2a00
Andrii Fedak <andrii_fedak@epam.com>
2023-10-14 21:46:40 +0300

    update

--- todo-list-back/index.js
+++ todo-list-back/index.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const express = require('express')
 const http = require('http');
 
@@ -5,32 +6,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3000
 
-app.get('/', async (_req, res) => {
-    const options = {
-        hostname: '169.254.169.254',
-        port: 80,
-        path: '/latest/meta-data/placement/availability-zone',
-        method: 'GET'
-    };
-    const req = http.request(options, (httpRes) => {
-        let data = '';
+app.get('/get-zone', async (_req, res) => {
+    const req = await axios.get('http://169.254.169.254/latest/meta-data/placement/availability-zone');
+    res.send(req.data)
+})
 
-        httpRes.on('data', (chunk) => {
-            data += chunk;
-        });
-
-        httpRes.on('end', () => {
-            res.send('Here is Availability Zone: ', data)
-        });
-    });
-
-    req.on('error', (error) => {
-        res.status(400).send('Error')
-    });
-
-    req.end();
+app.get('/health', async (_req, res) => {
+    res.send('Healthy')
 })
 
 app.listen(PORT, () => {
-    console.log('successfuly listening')
+    console.log('successfuly listening on port: ', PORT)
 })
