1bdafdb46e74d7aacc6cee294c7138f7c3e9d6c0
Andrii Fedak <andrii_fedak@epam.com>
2023-10-17 17:50:15 +0300

    fix

--- todo-list-back/config/db.js
+++ todo-list-back/config/db.js
@@ -1,11 +1,11 @@
 const dotenv = require('dotenv');
 
 module.exports.initDb = async () => {
-    dotenv.config({ path: './.env' })
     if(process.env.NODE_ENV !== 'production') {
-        dotenv.config({ path: './.db.env' })
+        dotenv.config({ path: './.env' })
         dotenv.config({ path: './.aws.env' })
     }
+    dotenv.config({ path: './.db.env' })
     
     const sequalize = require('../config/sequalize');
     await sequalize.authenticate()
